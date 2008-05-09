13e9558cb169618b3e32225554ccd728f297f938
jluehrs2 <jluehrs2@uiuc.edu>
2008-05-09 04:11:40 -0500

    add upload

---
+++ lib/App/Hiveminder/Command/Upload.pm
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+package App::Hiveminder::Command::Upload;
+use Moose;
+extends 'App::Hiveminder::Command';
+
+=head2 NAME
+
+App::Hiveminder::Command::Upload - Upload a previously downloaded file back to your todo list
+
+=cut
+
+sub command_names { qw/upload up ul/ }
+
+sub command {
+    my $self = shift;
+
+    return unless @_ > 0;
+    my $filename = shift;
+    $hm->upload_file($filename);
+    return $filename;
+}
+
+__PACKAGE__->meta()->make_immutable();
+1;
